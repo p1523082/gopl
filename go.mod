3814098a0f9ae6f255f726f3f72d0afdd62b1129
casio0128-dev <sweet.cafe0141@gmail.com>
2020-04-08 23:52:46 +0900

    configファイル内でport番号を指定できるように修正

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module Golang入門
 
 go 1.14
+
+require gopkg.in/ini.v1 v1.55.0
--- main.go
+++ main.go
@@ -5,14 +5,26 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"gopkg.in/ini.v1"
 )
 
-var time int = 0
+var time int
+var port string
 
 func main() {
+	// configファイル config.iniを読み込む
+	conf, err := ini.Load("./config/config.ini")
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	// webセクションのportを取得し、port変数に代入
+	port = conf.Section("web").Key("port").String()
+
 	// localhost:1323 に対するWebサーバを作成
 	// 受けたパラメータを表示する
-	http.HandleFunc("/", testHandler)
+	http.HandleFunc("/", incrementHandler)
 
 	// localhost:1323/html に対するWebサーバを作成
 	// １秒間隔でクライアント側のカウンターをインクリンメントする
@@ -20,7 +32,7 @@ func main() {
 
 	// port番号1323でWebサーバをスタートする
 	// 開始が失敗した場合、Log情報を書き込みアプリケーションを終了する
-	if err := http.ListenAndServe(":1323", nil); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
 		log.Fatal("err from ListenAndServe!")
 	}
 }
@@ -31,12 +43,15 @@ func printHTML(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("./sample.html"))
 
 	// htmlへ送るための構造体を宣言する
-	// param: Value int
+	// param1: Value int
+	// param2: Port string
 	// Valueはカウントアップ用の変数
 	data := &struct{
 		Value int
+		Port string
 	}{
 		Value: time,
+		Port: port,
 	}
 
 	// HTMLに対して定義した構造体を埋め込む
@@ -45,7 +60,7 @@ func printHTML(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func testHandler(w http.ResponseWriter, r *http.Request) {
+func incrementHandler(w http.ResponseWriter, r *http.Request) {
 	// アクセス一回につき、timeをインクリメントする
 	time += 1
 
